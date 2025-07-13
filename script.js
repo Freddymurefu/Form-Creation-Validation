4da9914b2cf5fb66512281ba882b00d5b66cb0e9
Fredrick Murefu <freddymurefu@gmail.com>
2025-07-13 17:40:34 +0300

    Wrote all JS validation code for the form

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM content loaded")
+
+const form = document.getElementById("registration-form")
+const feedbackDiv = document.getElementById("form-feedback")
+
+form.addEventListener("submit", function(event) {
+event.preventDefault()
+
+let userName = document.getElementById("username").value.trim()
+let email = document.getElementById("email").value.trim()
+let password = document.getElementById("password").value.trim()
+
+let isValid = true
+
+let messages = []
+
+if (userName.length < 3) {
+    isValid = false
+     messages.push("Please enter a valid username!")
+} 
+
+if (email.includes("@") === false || email.includes(".") === false) {
+    isValid = false
+    messages.push("Please enter a valid email address!")
+}
+
+if (password.length < 8) {
+    isValid = false
+    messages.push("Please enter a password of minimum 8 words")
+}
+
+if (isValid === true) {
+    feedbackDiv.textContent = "Registration successful!"
+    feedbackDiv.style.color = "#28a745"
+    feedbackDiv.style.display = "block"
+}else {
+     feedbackDiv.innerHTML = messages.join("<br>")
+     feedbackDiv.style.color = "#dc3545"
+     feedbackDiv.style.display = "block"
+}
+
+})
+
+})
